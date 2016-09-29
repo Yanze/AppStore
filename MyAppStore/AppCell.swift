fd14ee983eca184c31b15730662596c409696415
Yanze LIU <yanzeliu.france@gmail.com>
2016-09-29 16:37:20 -0700

    divider line

--- MyAppStore/AppCell.swift
+++ MyAppStore/AppCell.swift
@@ -14,12 +14,55 @@ class AppCell: UICollectionViewCell {
         setupViews()
     }
     
-    
      func setupViews() {
-        backgroundColor = .red
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(categoryLabel)
+        addSubview(priceLabel)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height:frame.width)
+        nameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
+        categoryLabel.frame = CGRect(x: 0, y: frame.width + 30, width: frame.width, height: 20)
+        priceLabel.frame = CGRect(x: 0, y: frame.width + 45, width: frame.width, height: 20)
     }
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "frozen")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 16
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Disney: Frozen"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Entertaiment"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    let priceLabel : UILabel = {
+        let label = UILabel()
+        label.text = "$3.99"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        return label
+    }()
+
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
 }
--- MyAppStore/CategoryCell.swift
+++ MyAppStore/CategoryCell.swift
@@ -21,23 +21,33 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .blue
+        collectionView.backgroundColor = .clear
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
     
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     
     func setupViews() {
-        backgroundColor = .black
+        backgroundColor = .clear
         addSubview(appsCollectionView)
+        addSubview(dividerLineView)
         
         appsCollectionView.delegate = self
         appsCollectionView.dataSource = self
         
         appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: appCellId)
         
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
+        
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView, "v1": dividerLineView]))
     }
     
     
@@ -45,17 +55,25 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         fatalError("init(coder:) has not been implemented")
     }
     
+    // appsCollectionView
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = appsCollectionView.dequeueReusableCell(withReuseIdentifier: appCellId, for: indexPath)
-        return cell
+        let appCollectionCell = appsCollectionView.dequeueReusableCell(withReuseIdentifier: appCellId, for: indexPath)
+        return appCollectionCell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 150, height: frame.height)
+        return CGSize(width: 100, height: frame.height)
     }
+    
+    // add margins
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 14, 0, 14)
+    }
+    
+    
     
 }
