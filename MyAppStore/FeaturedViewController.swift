1afaa94723c5105e070a41f6d9b42ff5f56bcd1e
Yanze LIU <yanzeliu.france@gmail.com>
2016-09-28 17:22:27 -0700

    collectionview added

---
+++ MyAppStore/FeaturedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyAppStore
+//
+//  Created by yanze on 9/28/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .orange
+    }
+
+    
+
+}
+
